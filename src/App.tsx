999a4efb710e4733115462cdc9474d6464d1c865
xdong <none@none.no>
2023-08-31 16:10:09 +0900

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	useEffect(() => {
+		const eventSource = new EventSource("http://localhost:3700/stream");
+		eventSource.onmessage = ({ data }) => {
+			console.log("New message", JSON.parse(data));
+		};
+	}, []);
+
+	return <div>hello</div>;
 }
 
 export default App;
